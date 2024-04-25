3b27b6a62b66a9bd90f2615c044a47ddf8a6aa54
pandrr <tom@tomkom.net>
2024-04-25 18:06:04 +0200

    example

--- some.glsl
+++ some.glsl
@@ -1,10 +1,12 @@
-{{MODULES_HEAD}}
+uniform float a;
+uniform float b, c;
 
-
+void main()
+{
     {{MODULE_COLOR}}
 
     #ifdef DISCARDTRANS
-        if(col.a<0.2) discard;
+        if(col.a<0.2) discard; // hallo! was?
     #endif
 
     outColor = col;
--- some_min.glsl
+++ some_min.glsl
@@ -1,4 +1,7 @@
-{{MODULES_HEAD}}
+uniform float a;
+uniform float b,c;
+void main()
+{
 {{MODULE_COLOR}}
 #ifdef DISCARDTRANS
 if(col.a<0.2)discard;
