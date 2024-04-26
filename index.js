f01619b9c5d38c221b39b3b5fa3d3b7e1e6f8e92
pandrr <tom@tomkom.net>
2024-04-26 14:08:37 +0200

    min

--- index.js
+++ index.js
@@ -12,38 +12,44 @@ for(let i=0;i<tokens.length-1;i++)
 {
 	const token=tokens[i];
 
-	if(i>0)
-    	{
-    		if(token.type=="line-comment") continue;
-    		if(token.type=="block-comment") continue;
+	if (i > 0)
+	{
+		if (token.type == "line-comment") continue;
+		if (token.type == "block-comment") continue;
 
-            if(token.type=="whitespace" && token.data=="\n" )
-                if(tokens[i-1].type=="line-comment" ) continue;
+		if (token.type == "whitespace" && token.data == "\n" && tokens[i - 1].type == "line-comment") continue;
 
-    		if(token.type=="whitespace")
-    		{
-    			token.data=token.data.replaceAll("\n\n","\n");
-    			token.data=token.data.replaceAll("\n\n","\n");
-    			token.data=token.data.replaceAll("\t"," ");
-    		 	token.data=token.data.replaceAll("   "," ");
-    		 	token.data=token.data.replaceAll("  "," ");
-    		 	token.data=token.data.replaceAll("\n\n","\n");
-    		}
+		if (token.type == "whitespace")
+		{
+			if (token.data.indexOf("\n")==0 && token.data.endsWith(" ")) token.data="\n";
 
-    		if(token.type=="float")
-    			while(token.data.indexOf(".")>0 && token.data.endsWith("0"))
-    				token.data=token.data.substring(0, token.data.length-1)
+			for (let j = 0; j < 3; j++)
+				token.data = token.data.replaceAll("\n\n", "\n");
 
-    		if(token.type=="whitespace" && token.data==" " )
-    		{
-    			if(tokens[i-1].type=="ident" && tokens[i+1].type=="ident") continue;
-    			if(tokens[i-1].type=="operator" && tokens[i+1].type=="ident") continue;
-    			if(tokens[i-1].type=="operator" && tokens[i+1].type=="float") continue;
-    			if(tokens[i-1].type=="operator" && tokens[i+1].type=="keyword") continue;
-    			if(tokens[i-1].type=="operator" && tokens[i+1].type=="operator") continue;
-    			if(tokens[i+1].type!="ident" && tokens[i+1].type!="keyword" && tokens[i-1].type!="ident" && tokens[i-1].type!="keyword" ) continue;
-    		}
-    	}
+			token.data = token.data.replaceAll("\t", " ");
+
+			 for (let j = 0; j < 3; j++)
+				 token.data = token.data.replaceAll("  ", " ");
+
+			 for (let j = 0; j < 2; j++)
+				 token.data = token.data.replaceAll("\n\n", "\n");
+		}
+
+		if (token.type == "float")
+			while (token.data.indexOf(".") > 0 && token.data.endsWith("0"))
+				token.data = token.data.substring(0, token.data.length - 1);
+
+		if (token.type == "whitespace" && token.data == " ")
+		{
+			if (tokens[i - 1].type == "ident" && tokens[i + 1].type == "ident") continue;
+			if (tokens[i - 1].type == "ident" && tokens[i + 1].type == "operator") continue;
+			if (tokens[i - 1].type == "operator" && tokens[i + 1].type == "ident") continue;
+			if (tokens[i - 1].type == "operator" && tokens[i + 1].type == "float") continue;
+			if (tokens[i - 1].type == "operator" && tokens[i + 1].type == "keyword") continue;
+			if (tokens[i - 1].type == "operator" && tokens[i + 1].type == "operator") continue;
+			if (tokens[i + 1].type != "ident" && tokens[i + 1].type != "keyword" && tokens[i - 1].type != "ident" && tokens[i - 1].type != "keyword") continue;
+		}
+	}
 
 	str+=token.data;
 }
